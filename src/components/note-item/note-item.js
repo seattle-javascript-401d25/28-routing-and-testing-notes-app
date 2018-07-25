6e053ff66aa45af9c4dd558eab755de04e7299fd
Noah Israel <noahmvf@gmail.com>
2018-07-25 09:19:30 -0700

    reconfigured files

--- src/components/note-item/note-item.js
+++ src/components/note-item/note-item.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Modal from '../modal/modal';
-import NoteForm from '../note-form/note-form';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Modal from '../modal/modal';
+// import NoteForm from '../note-form/note-form';
+// import Dashboard from '../dashboard/dashboard';
 
-export default class NoteItem
+// export default class NoteItem extends React.Component {
+
+//   render() {
+//     const { note, handleRemoveNote, handle }
+//   }
+// }
